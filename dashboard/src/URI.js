ef4c1019f90ebd78b726764fc7394365963bb5b3
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-10-07 14:05:42 +0530

    API Error

--- dashboard/src/URI.js
+++ dashboard/src/URI.js
@@ -1,4 +1,4 @@
-const API_SERVICE = "https://searchdataapi.herokuapp.com";
+const API_SERVICE = "https://newsearchsaasapipw.herokuapp.com";
 // const API_SERVICE = "http://localhost:5000";
 
 export { API_SERVICE };
