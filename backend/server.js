e7bcb9c941785b1285c35f84cc2343057c6cef30
Manav Nanwani <manav.nanwani@gmail.com>
2021-10-07 14:46:29 +0530

    Check

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
-//const BlogPost=require('./models/BlogPost')
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+
 const routes = require("./routes/api");
 
-const path = require("path");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ message: "HI" });
-});
-
-const cors = require("cors");
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(morgan("tiny"));
 app.use("/api", routes);
 
-app.listen(PORT, console.log(`Server is running at ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Welcome !!");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
--- dashboard/src/URI.js
+++ dashboard/src/URI.js
@@ -1,4 +1,4 @@
-const API_SERVICE = "https://newsearchsaasapipw.herokuapp.com";
+const API_SERVICE = "https://worksearchapp.herokuapp.com";
 // const API_SERVICE = "http://localhost:5000";
 
 export { API_SERVICE };
